c1521ec665f1cb1898d8de2945212e0fd132cf28
Michael <michael.taverner@gmail.com>
2020-11-10 21:47:11 +1100

    Intial commit - ETL in first stage The script picks up a hardcoded CSV, adds a UID column based on the columns that constitute "uniqueness" in a row and mashes them together into a UID for the purpose of avoiding duplication in the DB.

---
+++ DB_data_insertion.py
@@ -0,0 +1,91 @@
+import time
+from datetime import datetime
+from dotenv import load_dotenv
+import os
+import pandas as pd
+import csv
+import psycopg2 as pg
+from io import StringIO
+
+load_dotenv()
+
+start_time = time.time()
+
+# Define the file path, file name and prepared file path
+origin_file_path = '1. Original Data/trend_data_2020-05-02_to_2020-07-31.csv'
+file_name = origin_file_path.split('/')[-1]
+prepared_file_path = '2. Prepared Data/{}'.format(file_name)
+
+
+# Read CSV to DF, convert first col to 'yyyy-mm-dd' format, add empty ID col
+#my_parser = lambda x: datetime.strptime(x, "%d/%m/%Y")
+df = pd.read_csv(origin_file_path, parse_dates=[0])
+df['uid'] = df['Date'].map(str)+df['Country Code'].map(str)+df['Song ISRC'].map(str)+df['Provider'].map(str)
+
+#Check if file already exists in Prepared data - if so, delete
+#Write altered CSV to Prepared Data folder with same file name as origin
+if os.path.exists(prepared_file_path):
+    os.remove(prepared_file_path)
+    df.to_csv(prepared_file_path,index=False)
+else: df.to_csv(prepared_file_path,index=False)    
+
+
+# DB connection parameters
+hostname = os.environ.get('host'),
+database = os.environ.get('database')
+user = os.environ.get('user')
+password = os.environ.get('password')
+sslmode = 'disable'
+
+# Create connection and cursor
+conn_string = "host={} dbname={} user={} password={} sslmode={}".format(hostname[0],
+                                                                        database,
+                                                                        user,
+                                                                        password,
+                                                                        sslmode)
+conn = pg.connect(conn_string)
+cur = conn.cursor()
+
+
+# Define table name in DB
+table_name = 'trend_data'
+
+
+
+with open(prepared_file_path, 'r') as f:
+    reader = csv.reader(f)
+    next(reader) # Skip the header row.
+    for row in reader:
+        cur.execute(f""" INSERT INTO {table_name} 
+            VALUES (%s, %s, %s, %s,%s, %s, %s, %s,%s, %s, %s, %s,%s, %s, %s)
+            ON CONFLICT (uid)
+            DO NOTHING
+            """,
+        row)
+               
+
+
+conn.commit()
+rows_affected = cur.rowcount
+
+"""
+NEXT ITERATION: Figure out how to replace the dumb UID that is generated at the 
+DF stage.
+
+I wont be able to have the DB create the ID for me for the purpose of uniqueness, 
+because it needs to check for duplicates at the upload stage, OR it needs to 
+remove duplicates after the upload
+
+Given that every row needs to be unique, I might create a composite primary key
+of the appropriate columns in the DB, then do the ON CONFLICT check based on that
+
+"""
+
+
+cur.close()
+conn.close()
+
+
+print('Copied {} rows to {} in'.format(rows_affected,table_name),round((time.time() - start_time),2), 
+      'seconds after checking for duplicates')
+
